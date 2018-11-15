7f988f50da99d0c0b13a2f67602403cbf49abd19
virgil23 <virgil17@live.com>
2018-11-14 20:53:13 -0500

    Front end updates

--- javascript/logic.js
+++ javascript/logic.js
@@ -3,12 +3,15 @@
 // function that runs when page loads so changes will stay after a refresh
 $(function() {
   changeHeaderColor();
+  // changeMainColor();
   //scrollMarker();
   //mobileNav();
   // on scroll, run this function again, as the condition will change once scrolled
   $(window).scroll(changeHeaderColor);
+  // $(window).scroll(changeMainColor);
   // Run function again on any resize done
   $(window).resize(changeHeaderColor);
+  // $(window).resize(changeMainColor);
 });
 
 
@@ -17,21 +20,39 @@ $(function() {
 function changeHeaderColor() {
 
   nav = $('#nav');
+
+  brand = $('#brand');
+
   // brand = $('.navbar-brand');
 // These changes take place if DOM width is larger than tablets
   // if ( ($(window).scrollTop() > nav.height()) && (( $(window).width() > 998 )) ) {
-    if ( ($(window).scrollTop() > ($(window).height() - nav.height())) && (( $(window).width() > 998 )) ) {
+    if ( ($(window).scrollTop() > ($(window).height() - nav.height())) && (( $(window).width() > 908 )) ) {
     // nav.css("background-color", "rgba(0,0,0,0.8)");
     // nav.css("background-color", "#1a1a1a");
     nav.css({
-      "background-color": "#333",
-      "transition": "0.3s ease"
+      "background-color": "#333333"
     });
+
     // nav.addclass("fadeInDown");
   } else {
-    nav.css("background", "transparent");
+    nav.css({
+      "background-color": "transparent"
+    });
   }
+
 };
+
+function changeMainColor() {
+
+  name = $('.name');
+
+  if( $(window).scrollTop() > nav.height()) {
+    
+    name.css("color", "transparent");
+  } else {
+    name.css("color", "white;")
+  }
+}
 
 // function for changing word colors
 // nav link is the words on links
